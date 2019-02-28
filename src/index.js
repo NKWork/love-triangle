67061fb288a74c91fbbd91e56a255969729e1719
NKWork <NKwork95@yandex.by>
2019-02-28 20:59:20 +0300

    love triangles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(arr = []) {
+  let count=0;
+  for(let i=0;i<arr.length;i++){
+    if(arr[arr[arr[i]-1]-1]==i+1){
+      count = count+1;
+    }
+  }
+  return Math.floor(count/3);
 };
